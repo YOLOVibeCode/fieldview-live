5d20f362bc8085373babadba037bc607098a1d8b
Ricardo Vega <github@noctusoft.com>
2026-01-02 03:03:53 -0600

    fix(data-model): Avoid ESM dir imports; compile CommonJS for Node runtime

--- packages/data-model/src/index.ts
+++ packages/data-model/src/index.ts
@@ -1,6 +1,6 @@
 // @fieldview/data-model
 // Main entry point for data model package
 
-export * from './entities';
-export * from './schemas';
-export * from './utils';
+export * from './entities/index';
+export * from './schemas/index';
+export * from './utils/index';
